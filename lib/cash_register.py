4884f9fe799d413c75a18fd6b5917b503f0de882
Rene <racosta323@gmail.com>
2024-02-22 15:33:43 -0600

    discount message

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -2,24 +2,32 @@
 import ipdb
 
 class CashRegister:
-  items = []
-
-  def __init__(self, discount = 0, total = 0):
-    self.discount = discount
-    self.total = total
-    print("hello")
-    
-
-  def add_item(self, title, price, quantity=1):
-    self.title = title
-    self.price = price * quantity
-    self.total = self.price + self.total
   
+  def __init__(self, discount = 0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    
+    
+  def add_item(self, title, price, quantity=1):
+    updated_price = price * quantity
+    self.total = updated_price + self.total
+    number_of = quantity * title
+    each_title = [each for each in number_of]
+    ipdb.set_trace()
+    return self.items.append(title * quantity)
+    
+    # return CashRegister.items
+   
   def apply_discount(self):
-    self.discount = (self.discount/100) * self.total
-    self.total -= self.discount
-    return f'After the discount, the total comes to ${int(self.total)}.'
-    # ipdb.set_trace()
+    local_discount = (self.discount/100) * self.total
+    self.total -= local_discount
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      print(f"After the discount, the total comes to ${int(self.total)}.")
 
-cash_register = CashRegister(20, 1000)  
-print(cash_register.apply_discount())  
+new_register = CashRegister()
+new_register.add_item("eggs", 1.99, 2)
+
+# ipdb.set_trace()
